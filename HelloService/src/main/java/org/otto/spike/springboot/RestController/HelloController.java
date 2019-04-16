8ad8f561fccbe3baf80e33db56b7a9322c022e8e
umarakram1101 <umar.akram@accenture.com>
2019-04-16 11:21:27 +0200

    msg changed

--- HelloService/src/main/java/org/otto/spike/springboot/RestController/HelloController.java
+++ HelloService/src/main/java/org/otto/spike/springboot/RestController/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
   @GetMapping("/hello")
   public Hello getHelloWordObject() {
       Hello helloObj = new Hello();
-      helloObj.setMessage("Hi there..!! ");
+      helloObj.setMessage("Hi Barracuties..Let..Set..GO!! ");
       return helloObj;
   }
 }
